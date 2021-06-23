c222271ab531566c884fb94a9b20d8c52c04b0a1
Joaco Saralegui <joacosaralegui@gmail.com>
2021-06-23 11:40:37 -0300

    Priemra version de conversacion para extraer features. Muchos cambios en el flujo con rules para iterar por formularios

--- wizard-de-diseno/actions/actions.py
+++ wizard-de-diseno/actions/actions.py
@@ -8,8 +8,10 @@ from typing import Any, Text, Dict, List, Optional
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import FollowupAction
+from rasa_sdk.events import FollowupAction, EventType
 from rasa_sdk.forms import FormValidationAction
+from rasa_sdk.events import SlotSet
+
 
 from . import modelado
 
@@ -110,3 +112,122 @@ class ActionPregunta(Action):
         text =  tracker.latest_message['text']
         dispatcher.utter_message(text=modelado.get_pregunta(text))
         return[]
+
+class AskForRespuestaEntradaAction(Action):
+    def name(self) -> Text:
+        return "action_ask_respuesta_entrada"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        """
+        Se fija la pregunta que tiene que hacer de acuerdo a la Queue de preguntas que tenemos
+        para las entidades reconocidas como entradas 
+        """
+        preguntas_entrada = tracker.get_slot("preguntas_entrada")
+        if preguntas_entrada and len(preguntas_entrada) > 0:
+            dispatcher.utter_message(text=preguntas_entrada.pop(0))
+            if len(preguntas_entrada) == 0:
+                return [SlotSet('termino_entradas',True)]
+
+        return []
+
+class AskForRespuestaSalidaAction(Action):
+    def name(self) -> Text:
+        return "action_ask_respuesta_salida"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        """
+        Se fija la pregunta que tiene que hacer de acuerdo a la Queue de preguntas que tenemos
+        para las entidades reconocidas como salida 
+        """
+        preguntas_salida = tracker.get_slot("preguntas_salida")
+        if preguntas_salida and len(preguntas_salida) > 0:
+            dispatcher.utter_message(text=preguntas_salida.pop(0))
+            if len(preguntas_salida) == 0:
+                return [SlotSet('termino_salidas',True)]
+                
+        return []
+
+
+class ActionAnalizarEntradas(Action):
+
+    def name(self) -> Text:
+        return "action_analizar_entradas"
+
+    def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+        ) -> List[Dict[Text, Any]]:
+
+        text =  tracker.latest_message['text']
+        # TODO: ANALISIS
+        # Generar preguntas automagicamente para la entrada
+        return [SlotSet('termino_entradas',False),SlotSet('preguntas_entrada',["De que color es el caballo blanco de San Martín?"])]
+
+class ActionAnalizarSalidas(Action):
+
+    def name(self) -> Text:
+        return "action_analizar_salidas"
+
+    def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+        ) -> List[Dict[Text, Any]]:
+
+        text =  tracker.latest_message['text']
+        # TODO: ANALISIS
+        # Generar preguntas automagicamente para la salida
+        return [SlotSet('preguntas_salida',["De que color es el caballo blanco de San Martín?"])]
+
+
+class ActionGuardarEntrada(Action):
+
+    def name(self) -> Text:
+        return "action_guardar_entrada"
+
+    def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+        ) -> List[Dict[Text, Any]]:
+
+        text =  tracker.latest_message['text']
+        features = tracker.get_slot('features')
+        if not features:
+            features = [text]
+        else:
+            features.append(text)
+        
+        # TODO: unificar en unica accion action_guardar y usarlapara ambos casos
+        # Generar preguntas automagicamente
+        return [SlotSet('features',features)]
+
+class ActionGuardarSalida(Action):
+
+    def name(self) -> Text:
+        return "action_guardar_salida"
+
+    def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+        ) -> List[Dict[Text, Any]]:
+
+        text =  tracker.latest_message['text']
+        features = tracker.get_slot('features')
+        if not features:
+            features = [text]
+        else:
+            features.append(text)
+        
+        # Generar preguntas automagicamente
+        return [SlotSet('features',features)]
