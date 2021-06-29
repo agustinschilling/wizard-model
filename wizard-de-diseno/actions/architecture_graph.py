0ff19d378c5b256006c69748b271831d5bd2e290
gianluca <gcapozzo@alumnos.exa.unicen.edu.ar>
2021-06-29 17:34:07 -0300

    agregado metodo que remueve la entidad sistema y crea nuevos nodos para los arcos

--- wizard-de-diseno/actions/architecture_graph.py
+++ wizard-de-diseno/actions/architecture_graph.py
@@ -1,9 +1,13 @@
+import sys
+sys.path.append('/home/gianluca/PycharmProjects/wizard-model/wizard-de-diseno/actions/')
+from typing import Dict
+
 import pygraphviz as pgv
-import enchant 
-import os 
-from rasa_sdk.events import SlotSet
+import enchant
 from datetime import datetime
-from actions import mongo_client 
+import mongo_client
+import entities as entities_file
+
 
 class GraphManager:
     """
@@ -15,7 +19,7 @@ class GraphManager:
     hacer 1 actualizacion a partir de un unico requerimiento o elimiar algo
     No soporta multiples acciones a la vez
     """
-    
+
     def __init__(self, id):
         """
         Inicializa el Manager. 
@@ -36,19 +40,19 @@ class GraphManager:
         """
         Crear grafo nuevo
         """
-        return pgv.AGraph(strict=False,directed=True)
+        return pgv.AGraph(strict=False, directed=True)
 
     def save(self):
         """
         Almacenar nueva version de la arqui en mongodb
         """
-        return mongo_client.save_arqui(self.id, self.graph.to_string(), self.color_idx+1)
+        return mongo_client.save_arqui(self.id, self.graph.to_string(), self.color_idx + 1)
 
     def remove_last(self):
         mongo_client.remove_arqui(self.id)
         self.__init__(self.id)
 
-    def update_graph_with_new_entities(self,entities,intent):
+    def update_graph_with_new_entities(self, entities, intent):
         """
         De acuerdo a la estructura clasificada en el intent,
         cargamos las entidades de una manera específica 
@@ -59,65 +63,64 @@ class GraphManager:
         if intent == "star":
             self.update_graph_with_star_entities(entities)
         elif intent == "simple_chain" or intent == "double_chain":
-            self.update_graph_with_chained_entities(entities)  
+            self.update_graph_with_chained_entities(entities)
         elif intent == "star_and_double_chain":
-            cut_index = len(entities)-2
+            cut_index = len(entities) - 2
 
             if len(entities) > cut_index + 1:
-                self.update_graph_with_star_entities(entities[:cut_index+1])
-                self.update_graph_with_chained_entities(entities[cut_index:])  
+                self.update_graph_with_star_entities(entities[:cut_index + 1])
+                self.update_graph_with_chained_entities(entities[cut_index:])
         elif intent == "star_and_simple_chain":
-            cut_index = len(entities)-1
+            cut_index = len(entities) - 1
 
             if len(entities) > cut_index:
-                self.update_graph_with_star_entities(entities[:cut_index+1])
-                self.update_graph_with_chained_entities(entities[cut_index:])  
+                self.update_graph_with_star_entities(entities[:cut_index + 1])
+                self.update_graph_with_chained_entities(entities[cut_index:])
         else:
             nodes = [e for e in entities if e.is_node()]
             if "simple" in intent and len(nodes) > 1:
                 cut_index = entities.index(nodes[1])
-            elif "double" in intent  and len(nodes) > 2:
+            elif "double" in intent and len(nodes) > 2:
                 cut_index = entities.index(nodes[2])
-            else: 
-                cut_index = len(entities)-1
+            else:
+                cut_index = len(entities) - 1
 
             if len(entities) > cut_index:
-                self.update_graph_with_chained_entities(entities[:cut_index+1])
-                self.update_graph_with_star_entities(entities[cut_index:])  
-
+                self.update_graph_with_chained_entities(entities[:cut_index + 1])
+                self.update_graph_with_star_entities(entities[cut_index:])
+        self.delete_entity(entities_file.SYSTEM, {"shape": "triangle", "color": "blue"})
 
     def get_image_file(self):
         """
         Genera una imagen para representar el estado actual de la arquitectura
         """
-        filename = 'actions/images/file'+ str(datetime.now()) +'.png'
-        self.graph.layout(prog='dot') 
-        self.graph.draw(filename) 
+        filename = 'actions/images/file' + str(datetime.now()) + '.png'
+        self.graph.layout(prog='dot')
+        self.graph.draw(filename)
         return filename
 
-
-    def add_node(self,node):
+    def add_node(self, node):
         """
         Agregar nodo al grafo
         """
-        self.graph.add_node(node.name,color=node.get_color(),shape=node.get_shape())
+        self.graph.add_node(node.name, color=node.get_color(), shape=node.get_shape())
 
-    def update_name_if_similar_found_in_graph(self,node_to_add):
+    def update_name_if_similar_found_in_graph(self, node_to_add):
         """
         Si encontramos en el grafo un nodo con nombre similar al que queremos agregar, 
         le cambiamos el nombre al nodo a agregar para que sea el mismo del que ya esta
         """
         # TODO: si alguno incluye a otro entonces tmb los podriamos tomar como iguales
-        closest = {'node':None,'distance':3}
+        closest = {'node': None, 'distance': 3}
         for node_to_compare in self.graph.nodes():
             calculated_distance = enchant.utils.levenshtein(node_to_compare, node_to_add.name)
             if calculated_distance < closest['distance']:
-                closest['node'], closest['distance']= node_to_compare, calculated_distance
-        
+                closest['node'], closest['distance'] = node_to_compare, calculated_distance
+
         if closest['node'] != None:
             node_to_add.name = closest['node'].name
 
-    def get_closest_node(self,ref_entity,node_entities):
+    def get_closest_node(self, ref_entity, node_entities):
         """
         Dado una entidad de referencia buscar la entidad nodo mas cercana
         de acuerdo a la posicion en el texto del requerimiento
@@ -129,7 +132,7 @@ class GraphManager:
         else:
             for node in node_entities[1:]:
                 if ref_entity.end < node.start:
-                    if abs(ref_entity.start - prev_node.end) <  abs(ref_entity.end - node.start):
+                    if abs(ref_entity.start - prev_node.end) < abs(ref_entity.end - node.start):
                         return prev_node
                     else:
                         return node
@@ -137,13 +140,13 @@ class GraphManager:
                     prev_node = node
             return prev_node
 
-    def enough_nodes(self,nodes):
+    def enough_nodes(self, nodes):
         """
         Chequeo si tengo al menos dos nodos,sino no puedo armar nada en el grafo 
         """
         return len(nodes) > 1
 
-    def update_edge_label(self,edge,entity):
+    def update_edge_label(self, edge, entity):
         """
         actualizar un label de arista con el nombre de una entidad
         Si ya tiene una, agregar la nueva
@@ -152,35 +155,35 @@ class GraphManager:
         if entity.name not in label:
             edge.attr['label'] += ", " + entity.name
 
-    def add_labeled_edge(self,node1,node2,entity):
+    def add_labeled_edge(self, node1, node2, entity):
         """
         Agregar una arista entre dos nodos. 
         Si la arista ya existe, simplemente actualizar la etiqueta
         """
-        self.graph.add_edge(node1.name, node2.name, label=entity.name, color=COLORS[self.color_idx])  
+        self.graph.add_edge(node1.name, node2.name, label=entity.name, color=COLORS[self.color_idx])
 
-    def add_edge(self,node1,node2):
+    def add_edge(self, node1, node2):
         """
         Agregar arista sin etiqueta entre dos nodos
         """
-        self.graph.add_edge(node1.name, node2.name, color=COLORS[self.color_idx])  
+        self.graph.add_edge(node1.name, node2.name, color=COLORS[self.color_idx])
 
-    def find_nodes_connected_by_event(self,event,nodes):   
+    def find_nodes_connected_by_event(self, event, nodes):
         """
         Dado un evento, definir que dos nodos/entidades conecta
         Se hace en relacion a la posicion de las entidades en el texto
-        """  
+        """
         i = 0
-        first_node,second_node = nodes[i],nodes[i+1]
-        while i < len(nodes)-2:
+        first_node, second_node = nodes[i], nodes[i + 1]
+        while i < len(nodes) - 2:
             if event.end < second_node.start:
                 return first_node, second_node
             else:
                 i += 1
-                first_node,second_node = nodes[i],nodes[i+1]
-        return first_node,second_node           
+                first_node, second_node = nodes[i], nodes[i + 1]
+        return first_node, second_node
 
-    def update_nodes(self,nodes):
+    def update_nodes(self, nodes):
         """
         Agrega nodos pero primero actualizar los nombres si ya estan en el grafo
         """
@@ -188,32 +191,32 @@ class GraphManager:
             self.update_name_if_similar_found_in_graph(node)
             self.add_node(node)
 
-    def update_events(self,nodes,entities):
+    def update_events(self, nodes, entities):
         """
         Para los nodos 
         """
         events = [e for e in entities if e.is_event()]
         for event in events:
-            node1,node2 = self.find_nodes_connected_by_event(event,nodes)          
-            self.add_labeled_edge(node1,node2,event)
-    
-    def update_properties(self,nodes,entities):
+            node1, node2 = self.find_nodes_connected_by_event(event, nodes)
+            self.add_labeled_edge(node1, node2, event)
+
+    def update_properties(self, nodes, entities):
         properties = [e for e in entities if e.is_property()]
         for property_node in properties:
             if property_node not in nodes:
-                closest_node = self.get_closest_node(property_node,nodes)
+                closest_node = self.get_closest_node(property_node, nodes)
                 self.add_node(property_node)
-                self.add_edge(closest_node, property_node)  
+                self.add_edge(closest_node, property_node)
 
-    def connect_unconnected_nodes(self,nodes):
+    def connect_unconnected_nodes(self, nodes):
         prev_node = nodes[0]
         for node in nodes[1:]:
-            if not self.graph.has_edge(prev_node.name,node.name):
-                self.add_edge(prev_node,node)
-                self.add_edge(node,prev_node)
+            if not self.graph.has_edge(prev_node.name, node.name):
+                self.add_edge(prev_node, node)
+                self.add_edge(node, prev_node)
             prev_node = node
 
-    def update_graph_with_chained_entities(self,entities):
+    def update_graph_with_chained_entities(self, entities):
         nodes = [e for e in entities if e.is_node()]
         if not self.enough_nodes(nodes):
             nodes = [e for e in entities if e.is_node() or e.is_property()]
@@ -221,31 +224,55 @@ class GraphManager:
             return
 
         self.update_nodes(nodes)
-        self.update_events(nodes,entities)
-        self.update_properties(nodes,entities)
+        self.update_events(nodes, entities)
+        self.update_properties(nodes, entities)
         self.connect_unconnected_nodes(nodes)
 
-
-    def update_graph_with_star_entities(self,entities):
+    def update_graph_with_star_entities(self, entities):
         nodes = [e for e in entities if e.is_node()]
         if not self.enough_nodes(nodes):
             nodes = [e for e in entities if e.is_node() or e.is_property()]
-        
+
         if not self.enough_nodes(nodes):
             return
-        
+
         self.update_nodes(nodes)
-        self.update_properties(nodes,entities)
-        
+        self.update_properties(nodes, entities)
+
         main_node = nodes[0]
         for idx, entity in enumerate(entities[:-1]):
-            next_entity = entities[idx+1]
+            next_entity = entities[idx + 1]
             if entity.is_event() and next_entity.is_node() or next_entity.is_property() and next_entity != main_node:
-                self.add_labeled_edge(main_node,next_entity,entity)
+                self.add_labeled_edge(main_node, next_entity, entity)
 
         for node in nodes[1:]:
-            if not self.graph.has_edge(main_node.name,node.name):
-                self.add_edge(main_node,node)
+            if not self.graph.has_edge(main_node.name, node.name):
+                self.add_edge(main_node, node)
+
+    def delete_entity(self, node_to_delete_name: str, new_node_attr: Dict):
+        """
+        Delete @node_to_delete_name node and create new nodes for labels
+        @requires no in_neighbours and labels on each edge from @node_to_delete_name
+
+        @param node_to_delete_name: name of the node to delete in the graph
+        @param new_node_attr: new node attributes, color, shape, etc
+
+        @author: gcapozzo
+        """
+        # TODO check multiple edges
+        to_delete_node = self.graph.get_node(node_to_delete_name)
+        # no in_neighbours
+        if len(self.graph.in_neighbors(to_delete_node)) is 0:
+            # out_neighbours with label
+            for out_node in self.graph.out_neighbors(to_delete_node):
+                if self.graph.get_edge(to_delete_node, out_node).attr['label'] is None:
+                    return
+            for out_node in self.graph.out_neighbors(to_delete_node):
+                old_edge = self.graph.get_edge(to_delete_node, out_node)
+                self.graph.add_node(old_edge.attr['label'], **new_node_attr)
+                self.graph.add_edge(old_edge.attr['label'], out_node, color=old_edge.attr['color'])
+                self.graph.remove_edge(to_delete_node, out_node)
+            self.graph.remove_node(to_delete_node)
 
 
 COLORS = [
@@ -255,7 +282,7 @@ COLORS = [
     "brown2",
     "chartreuse",
     "chocolate",
-    "darkgreen"	,
+    "darkgreen",
     "darkblue",
     "darkolivegreen1",
     "deeppink",
