e0fa401eb4a511fed8c9d6662bea3b62619d3179
gianluca <gcapozzo@alumnos.exa.unicen.edu.ar>
2021-07-09 12:34:32 -0300

    creada funcionalidad de comparar preguntas generadas de una frase con preguntas hechas y por hacer

--- wizard-de-diseno/actions/modelado.py
+++ wizard-de-diseno/actions/modelado.py
@@ -1,6 +1,8 @@
+import itertools
 from string import Template
 from typing import List, Dict
 
+import enchant.utils
 from rasa.nlu.model import Interpreter
 from .entities import Entity
 from .architecture_graph import GraphManager
@@ -164,8 +166,6 @@ def get_questions(text: str, asked: List[Question], remaining: List[Question]) -
     :return: list of NEW questions
     """
 
-    #TODO use asked and remaining
-
     # Genera preguntas para un texto
     intent, entities = parse_req(text)
     print("Intent:" + intent)
@@ -176,59 +176,100 @@ def get_questions(text: str, asked: List[Question], remaining: List[Question]) -
         entities_tagged[entity.category].append(entity)
 
     preguntas = []
-    if len(entities_tagged["EVENT"]) is 1:
-        # simple chain
-        if len(entities_tagged["MODEL"]) is 2:
-            preguntas.append(Question("Contame de donde surge $model1 o cuál te imaginas que es su función",
-                                      {"model1": entities_tagged["MODEL"][0]}))
-            preguntas.append(Question("Contame de donde surge $model2 o cuál te imaginas que es su función",
-                                      {"model2": entities_tagged["MODEL"][1]}))
-        if len(entities_tagged["MODEL"]) is 1 and len(entities_tagged["COMPONENT"]) is 1:
-            preguntas.append(Question("Contame de donde surge $model1 o cuál te imaginas que es su función",
-                                      {"model1": entities_tagged["MODEL"][0]}))
-            preguntas.append(Question("Es necesario hacer algun otro uso del $component1",
-                                      {"component1": entities_tagged["COMPONENT"][0]}))
-
-        # falta un elemento para cadena simple
-        if (len(entities_tagged["MODEL"]) is 1) and (len(entities_tagged["COMPONENT"]) is 0):
-            preguntas.append(Question("El $event1 de $model1 de donde o con que debe hacerse?",
-                                      {"event1": entities_tagged["EVENT"][0],
-                                       "model1": entities_tagged["MODEL"][0]}))
-        if (len(entities_tagged["MODEL"]) is 0) and (len(entities_tagged["COMPONENT"]) is 1):
-            preguntas.append(Question("El $event1 que interactua con $component1 que debe resultado genera?",
-                                      {"event1": entities_tagged["EVENT"][0],
-                                       "component1": entities_tagged["COMPONENT"][0]}))
-    return preguntas
-
-#Codigo de joaco
-"""
-    preguntas_de_entidades = {}
-    preguntas = []
     for i, entity in enumerate(entities):
         if entity.category == "MODEL":
-            preguntas_de_entidades[entity.name] = i
-            preguntas.append("Contame de donde surge " + entity.name + " o cuál te imaginas que es su función")
+            preguntas.append(Question("Contame de donde surge $model o cuál te imaginas que es su función",
+                                      {"model": entity}))
             # preguntas.append("Que propiedades estarían asociadas a " + entity.name)
             # preguntas.append("Cuál sería el proceso para obtener " + entity.name)
         elif entity.category == "EVENT":
             if i + 1 < len(entities) and entities[i + 1].category != "EVENT":
-                preguntas.append(
-                    "Cómo implementarias el proceso que involucra " + entity.name + " " + entities[i + 1].name)
+                preguntas.append(Question("Cómo implementarias el proceso que involucra $event $entity",
+                                          {"event": entity, "entity": entities[i + 1]}))
             else:
-                preguntas.append("Con que elementos se relaciona el proceso de " + entity.name + " y cómo lo hace?")
+                preguntas.append(Question("Con que elementos se relaciona el proceso de $event y cómo lo hace?",
+                                          {"event": entity}))
         elif entity.category == "COMPONENT":
-            preguntas.append("Cómo se relaciona " + entity.name + " con las otras partes del sistema?")
+            preguntas.append(Question("Cómo se relaciona $component con las otras partes del sistema?",
+                                      {"component": entity}))
 
-    model_entities = [e.name for e in entities if e.category == "MODEL"]
+    model_entities = [e for e in entities if e.category == "MODEL" or e.category == "COMPONENT"]
     if len(model_entities) > 1:
-        if "chain" in intent:
-            preguntas.append(
-                "Explicame la relación entre " + model_entities[-2] + " y " + model_entities[-1] + " si existe")
-        else:
-            preguntas.append("Cómo explicarías la relación entre a " + model_entities[0] + " y " + model_entities[1])
+        for e1, e2 in itertools.combinations(model_entities, 2):
+            preguntas.append(Question("Contame mas informacion de como se relacionan $e1 y $e2",
+                                      {"e1": e1, "e2": e2}))
+    print([x.get_filled_question() for x in preguntas])
+    return remove_repeated_questions(preguntas, asked, remaining, 0.8)
 
-    guardar_prediccion(text, entities, intent, preguntas)
-"""
+
+def check_similar_words(word1, word2, ratio) -> bool:
+    if word1 in word2 or word2 in word1:
+        return True
+    # TODO mas L-gante
+    divider = word2
+    if len(word1) < len(word2):
+        divider = word1
+    #pocos cambios dan una distancia baja
+    if (1 - enchant.utils.levenshtein(word1, word2)/divider) > ratio:
+        print(word1+" es similar a "+word2)
+        return True
+
+    return False
+
+
+def check_similar_entities(old_q_ent: List[Entity], new_q_ent: List[Entity], ratio: float) -> bool:
+    """
+    check if two list of entities have a similarity, requires same category and @ratio similar name
+    :param old_q_ent:
+    :param new_q_ent:
+    :param ratio: ratio of similarity to consider similar
+    :return: true if similar, false otherwise
+    """
+    same_entities = 0
+    for q_old in old_q_ent:
+        for q_new in new_q_ent:
+            if q_old.category is q_new.category and check_similar_words(q_old.name, q_new.name, ratio):
+                same_entities += 1
+
+    # compare ratio with smaller list
+    if same_entities > 0:
+        # less entities in old
+        if len(old_q_ent) < len(new_q_ent):
+            if len(old_q_ent) / same_entities >= ratio:
+                return True
+        # more or equal in old
+        else:
+            if len(new_q_ent) / same_entities >= ratio:
+                return True
+
+    return False
+
+
+def remove_repeated_questions(new_q: List[Question],
+                              asked: List[Question],
+                              remaining: List[Question],
+                              ratio: float) -> List[Question]:
+    # TODO el metodo deberia solo recibir una lista que combine asked y remaining porque no se usan por separado
+    """
+    removes from a list a questions that are similar to asked and remaining with a ratio
+    :param ratio: ratio of similarity
+    :param new_q:
+    :param asked:
+    :param remaining:
+    :return: questions of new_q that arent similar
+    """
+    cleared_q = []
+    for q in new_q:
+        new_q_ent = q.get_used_entities()
+        for old_q in itertools.chain(asked, remaining):
+            old_q_ent = old_q.get_used_entities()
+            if not check_similar_entities(old_q_ent, new_q_ent, ratio):
+                cleared_q.append(q)
+            # TODO borrar print de debug
+            else:
+                print("detecto similar a: " + q.get_filled_question() + "y" + old_q.get_filled_question())
+    print([x.get_filled_question() for x in cleared_q])
+    return cleared_q
 
 
 #
